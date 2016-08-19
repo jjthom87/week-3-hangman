e1f084ec2d2e75cb73c2c0518d49ca8e1cfb5ab2
Jared Thomas <jjthom87@yahoo.com>
2016-08-19 05:30:53 -0400

    finally finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -53,53 +53,47 @@ $(document).ready(function(){
         }
   			boardGame();
   		
-  		document.onkeyup = function(event) { 
-				var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-        if (split.indexOf(userGuess) > -1){
-				for (var i = 0; i < random.word.length; i++){
-					if (random.word[i] == userGuess){
-            $('#message').text("Yea Brah!")
-						blankWord[i] = userGuess;
-            $('#word').text("Word: " + blankWord.join(" "));
-            guessedLetters.push(userGuess);
-          } 
-				}
-      } else {
-        guessesLeft--;
-        wrongLetters.push(userGuess);
-        $('#guesses').text("Guesses: " + wrongLetters);
-        $('#message').text("Wrong! Guess Again.");
-      }
-      // if (userGuess < 'a' || userGuess > 'z'){
-      //   wrongLetters.splice(0,1);
-      //   return alert("That's not a letter");
-      // }
-      if (random.word === blankWord.join('')) {
-        $('#dynImg').remove();
-        $('#wins').text("Wins: " + wins++);
-        $('#message').text("You're a Winner!")
-        ansPhoto = $('<img id="dynImg">');
-        ansPhoto.attr('src', random.pic).height(300).width(300);
-        $('#picture').append(ansPhoto).fadeIn(2500).fadeOut(2500);
-        audio.play();
-        boardGame();
-      };
-      if (guessesLeft <= 0) {
-        $('#dynImg').remove();
-        $('#losses').text("Losses: " + losses++);
-        $('#message').text("You're a total fucking loser");
-        ansPhoto = $('<img id="dynImg">');
-        ansPhoto.attr('src', '../images/loser.jpg').height(300).width(300);
-        $('#picture').append(ansPhoto).fadeIn(2500).fadeOut(2500);
-        audioLoser.play();
-        boardGame();
-      } 
-      // for(var i = 0; i < random.word.length; i++){
-      //    if ((guessedLetters.indexOf(userGuess) != -1) || (wrongLetters.indexOf(userGuess) != -1)){
-      //     return alert("Letter Already Guessed");
-      //   }
-      // }
-      $('#guessesLeft').text("Guesses left: " + guessesLeft);
-		};
-
+  document.onkeyup = function(event) { 
+  	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    if ((guessedLetters.indexOf(userGuess) > -1) || (wrongLetters.indexOf(userGuess) > -1)){
+        return alert("Letter Already Guessed");
+        } else if (userGuess < 'a' || userGuess > 'z'){
+        return alert("That's not a letter");
+      } else if (split.indexOf(userGuess) > -1){
+  		for (var i = 0; i < random.word.length; i++){
+  			if (random.word[i] == userGuess){
+          $('#message').text("Yea Brah!")
+  				blankWord[i] = userGuess;
+          $('#word').text("Word: " + blankWord.join(" "));
+          guessedLetters.push(userGuess);
+        }
+  		}
+    } else {
+      guessesLeft--;
+      wrongLetters.push(userGuess);
+      $('#guesses').text("Guesses: " + wrongLetters);
+      $('#message').text("Wrong! Guess Again.");
+    }
+    if (random.word === blankWord.join('')) {
+      $('#dynImg').remove();
+      $('#wins').text("Wins: " + wins++);
+      $('#message').text("You're a Winner!")
+      ansPhoto = $('<img id="dynImg">');
+      ansPhoto.attr('src', random.pic).height(300).width(300);
+      $('#picture').append(ansPhoto).fadeIn(2500).fadeOut(2500);
+      audio.play();
+      boardGame();
+    };
+    if (guessesLeft <= 0) {
+      $('#dynImg').remove();
+      $('#losses').text("Losses: " + losses++);
+      $('#message').text("You're a total fucking loser");
+      ansPhoto = $('<img id="dynImg">');
+      ansPhoto.attr('src', '../images/loser.jpg').height(300).width(300);
+      $('#picture').append(ansPhoto).fadeIn(2500).fadeOut(2500);
+      audioLoser.play();
+      boardGame();
+    } 
+    $('#guessesLeft').text("Guesses left: " + guessesLeft);
+  };
 });
